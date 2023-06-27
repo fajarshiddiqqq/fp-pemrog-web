44a62ef2f9683cd0eb9fa562c9760487cb61df39
unknown <farhanhidayatul2002@gmail.com>
2023-06-28 06:27:31 +0700

    pesan sudah samoai

--- src/history.php
+++ src/history.php
@@ -15,28 +15,28 @@
 
 <body>
     <?php include './components/navbar.php'; ?>
-    <table>
+    <table class="border-collapse w-2/3 mx-auto">
         <tr>
-            <th>No Booking</th>
-            <th>No Route</th>
-            <th>User Detail</th>
-            <th>Booking Date</th>
-            <th>Booking Expired</th>
-            <th>Booking_status</th>
-            <th>booking_token</th>
-            <th>Detail Tiket</th>
-
+            <th class="border border-slate-600 px-4 py-2">No Booking</th>
+            <th class="border border-slate-600 px-4 py-2">No Route</th>
+            <th class="border border-slate-600 px-4 py-2">User Detail</th>
+            <th class="border border-slate-600 px-4 py-2">Booking Date</th>
+            <th class="border border-slate-600 px-4 py-2">Booking Expired</th>
+            <th class="border border-slate-600 px-4 py-2">Booking_status</th>
+            <th class="border border-slate-600 px-4 py-2">booking_token</th>
+            <th class="border border-slate-600 px-4 py-2">Detail Tiket</th>
         </tr>
-        <tr>
-                <td><?php echo $dataBooking['booking_log_id'] ?> </td>
-                <td><?php echo $dataBooking['route_id'] ?> </td>
-                <td><?php echo $dataBooking['user_detail_id'] ?> </td>
-                <td><?php echo $dataBooking['booking_date'] ?></td>
-                <td><?php echo $dataBooking['booking_expired'] ?></td>
-                <td><?php echo $dataBooking['booking_status'] ?></td>
-                <td><?php echo $dataBooking['booking_token'] ?></td>
-                <td><a href="ongoing.php?booking_log=<?php echo $dataBooking['booking_log_id'] ?>">Lihat</a></td>
+        <tr class="bg-gray-200">
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['booking_log_id'] ?> </td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['route_id'] ?> </td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['user_detail_id'] ?> </td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['booking_date'] ?></td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['booking_expired'] ?></td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['booking_status'] ?></td>
+            <td class=" text-center px-4 py-2"><?php echo $dataBooking['booking_token'] ?></td>
+            <td class=" text-center px-4 py-2"><button class="bg-blue-500 text-black px-4 py-2 mt-4"><a href="ongoing.php?booking_log=<?php echo $dataBooking['booking_log_id'] ?>">Lihat</a></button></td>
         </tr>
+    </table>
 </body>
 
 </html>
