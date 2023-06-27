d608ad9da3c6085b6b7733bba90ecc920a1cdaab
fajarshiddiqqq <fajarshiddiqqq@gmail.com>
2023-06-27 12:14:17 +0700

    menambah halaman tambah data gunung di admin

--- src/mountaindetail.php
+++ src/mountaindetail.php
@@ -26,6 +26,7 @@ while ($row = $queryRute->fetch_assoc()) {
             <th>Nama</th>
             <th>Quota</th>
             <th>Recent</th>
+            <th>Distance</th>
             <th>Address</th>
             <th>Status</th>
             <th>Price</th>
@@ -35,15 +36,16 @@ while ($row = $queryRute->fetch_assoc()) {
         </tr>
         <tr>
             <?php foreach ($dataRute as $key => $value) : ?>
-                <td><?php echo $key + 1 ?><br></td>
-                <td><?php echo $value['mountain_id'] ?><br> </td>
-                <td><?php echo $value['route_name'] ?><br> </td>
-                <td><?php echo $value['route_quota'] ?><br> </td>
-                <td><?php echo $value['route_recent'] ?><br></td>
-                <td><?php echo $value['route_address'] ?><br></td>
-                <td><?php echo $value['route_status'] ?><br></td>
-                <td><?php echo $value['route_price'] ?><br></td>
-                <td><?php echo $value['route_img'] ?><br></td>
+                <td><?php echo $key + 1 ?></td>
+                <td><?php echo $value['mountain_id'] ?> </td>
+                <td><?php echo $value['route_name'] ?> </td>
+                <td><?php echo $value['route_quota'] ?> </td>
+                <td><?php echo $value['route_recent'] ?></td>
+                <td><?php echo $value['route_distance'] ?> km</td>
+                <td><?php echo $value['route_address'] ?></td>
+                <td><?php echo $value['route_status'] ?></td>
+                <td><?php echo $value['route_price'] ?></td>
+                <td><?php echo $value['route_img'] ?></td>
                 <td><a href="routedetail.php?route=<?php echo $value['route_id'] ?>">Lihat</a></td>
             <?php endforeach; ?>
 </body>
